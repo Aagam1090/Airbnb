c08c68163d12e16d984d2f29317cc8ccdb4e2449
Aagam Bharat Shah <aagamshah1090@gmail.com>
2024-04-13 16:43:34 -0700

    Added Validation Logic

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -14,17 +14,32 @@ export class SearchComponent {
   constructor(private formBuilder: FormBuilder) {
     this.registrationForm = this.formBuilder.group({
       name: [''],
-      city: [''],
-      priceMin: [''],
-      priceMax: [''],
-      bedrooms: [''],
-      people: [''],
-      amenities: ['']
-    });
+      city: ['', Validators.required],
+      priceMin: ['', [Validators.required, Validators.min(0)]],
+      priceMax: ['', [Validators.required, Validators.min(0)]],
+      bedrooms: ['', [Validators.min(1), Validators.max(30)]],
+      people: ['', [Validators.min(1), Validators.max(30)]],
+      amenities: [this.getDefaultAmenities()]
+    }, { validators: this.priceRangeValidator });
+  }
+
+  priceRangeValidator(form: FormGroup): { [key: string]: any } | null {
+    const min = form.get('priceMin')?.value || 0;
+    const max = form.get('priceMax')?.value || 0;
+    return min !== null && max !== null && min <= max ? null : { 'priceRangeInvalid': true };
+  }
+  getDefaultAmenities(): string[] {
+    // Return an empty array if no default value is desired
+    // or return an array with default selected values
+    return []; // This sets the amenities to have no pre-selected values
   }
 
   submitForm() {
-    console.log(this.registrationForm.value);
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    } else {
+      console.error('Form is not valid');
+    }
   }
 
 }
